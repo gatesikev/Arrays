f062d4844f0f3aea51794f4de8324f99d343b2ca
gatesikev <gatesikev1@gmail.com>
2026-02-17 15:05:15 -0800

    Question one

---
+++ index.js
@@ -0,0 +1,20 @@
+// Exercise one: arrray initialization and property access
+const productCategories= ["Books", "Electronics", "Clothing", "Home & Kitchen", "Toys & Games"];
+//a
+console.log(productCategories[0]);
+//b
+console.log(productCategories[productCategories.length - 1]);
+//c
+console.log(productCategories.length);
+//d
+productCategories[1]= "Pens";
+//e
+console.log(productCategories);
+
+
+//Exercise two: array methods
+const shoppingCart = ["Laptop", "Headphones", "Mouse", "Keyboard"];
+
+
+
+
