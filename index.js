f3c71407bfc2087c43bf0f5722e4d6f61b427ab6
gatesikev <gatesikev1@gmail.com>
2026-02-17 15:50:19 -0800

    Question three

--- index.js
+++ index.js
@@ -35,8 +35,25 @@ console.log(invetory);
 invetory.splice(1,1,25,35);
 console.log(invetory);
 
+//Exercise three: Shallow Copying of Arrays and the reference trap
 
 
+//a
+const originalScores=[90,85,78];
+//b
+const referenceCopy= originalScores; // reference copy, not a new array
+console.log(referenceCopy);
+//c 
+referenceCopy[0]= 100;
+console.log("Original Scores:", originalScores);
+console.log("Reference Copy:", referenceCopy);
+
+//d
+const spreadCopy= [...originalScores]; // creates a new array with the same elements
+//e
+spreadCopy[0]= 50;
+console.log("Original Scores:", originalScores);
+console.log("Spread Copy:", spreadCopy);
 
 
 
