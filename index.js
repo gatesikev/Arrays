658f07cc0b0ffe6aabc8ef6576a78b65d1d987a4
gatesikev <gatesikev1@gmail.com>
2026-02-17 15:16:08 -0800

    Question 2

--- index.js
+++ index.js
@@ -12,8 +12,30 @@ productCategories[1]= "Pens";
 console.log(productCategories);
 
 
-//Exercise two: array methods
-const shoppingCart = ["Laptop", "Headphones", "Mouse", "Keyboard"];
+//Exercise two: Push, Pop, Shift, Unshift, Splice
+let invetory=[10,20,30];
+
+//a
+invetory.push(40);
+console.log(invetory);
+
+//b
+invetory.pop(40);
+console.log(invetory);
+
+//c
+invetory.unshift(0);
+console.log(invetory);
+
+//d
+invetory.shift(0);
+console.log(invetory);
+
+//e
+invetory.splice(1,1,25,35);
+console.log(invetory);
+
+
 
 
 
