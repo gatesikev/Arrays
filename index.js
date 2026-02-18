1323558470bd360a3b80f0451b61857b92ff7a13
gatesikev <gatesikev1@gmail.com>
2026-02-17 16:52:01 -0800

    Question 5

--- index.js
+++ index.js
@@ -68,7 +68,24 @@ console.log(studentNames.lastIndexOf("Alice"));
 //c
 console.log(studentNames.includes("Charlie"));
 //d
-console.log(studentNames.includes("Eve"));
+console.log(studentNames.includes("Eve")); 
+
+//Exercise 5: Advanced Searching with find and findIndex
+const products= [
+    { id: 1, name: "Laptop", price: 999 },
+    { id: 2, name: "Smartphone", price: 499 },
+    { id: 3, name: "Tablet", price: 299 },
+    { id: 4, name: "Headphones", price: 199 },
+    { id: 5, name: "Speaker", price: 1500 }
+];
+
+//a 
+let foundProduct= products.find(product => product.id === 2); //"==="strictly equal operators checks datatypes and "== "compares the datatypes and values
+console.log(foundProduct);
+
+//b
+let foundIndex= products.findIndex(product => product.price > 1000);
+console.log(foundIndex); // returns -1 if no product is found with price greater than 1000
 
 
 
