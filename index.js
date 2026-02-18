409d0d15023a9d53199f66198b3190d67c540c1e
gatesikev <gatesikev1@gmail.com>
2026-02-18 00:12:59 -0800

    Question 12

--- index.js
+++ index.js
@@ -157,4 +157,18 @@ console.log(passingScores);
 const average = testScores.reduce((sum, score) => sum + score, 0) / testScores.length;
 const belowAverageScores = testScores.filter(score => score < average);
 console.log(belowAverageScores);
-                                                                                                                                      
+                                            
+
+//Exercise 12: Aggregating Data(reduce)
+const pricesUSD2 = [10.50, 20.00, 5.25];
+//a
+const total = pricesUSD2.reduce((sum, price) => sum + price, 0);
+console.log(total);
+const totalCost = pricesUSD2.reduce((sum, price) => sum + price, 0);
+console.log(`Total cost: $${totalCost.toFixed(2)}`);
+//b
+const maxPrice = pricesUSD2.reduce((max, price) => price > max ? price : max);
+console.log(`Max price: $${maxPrice.toFixed(2)}`);
+//c
+const countAbove15 = pricesUSD2.reduce((count, price) => price > 15 ? count + 1 : count, 0);
+console.log(`Prices greater than $15: ${countAbove15}`);
