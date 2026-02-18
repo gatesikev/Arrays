10c787799f2b7f0251c5a9482b28805dfc286bbc
gatesikev <gatesikev1@gmail.com>
2026-02-17 23:07:39 -0800

    Question 8

--- index.js
+++ index.js
@@ -113,3 +113,17 @@ console.log(descendingSorted);
 const reversed = [...dataPoints].reverse(); // reverses the original array order
 console.log(reversed);
 
+//Exercise 8: Concatenation, Joining, and Slicing
+const arr1=["A", "B", ];
+const arr2=["C", "D",];
+const sentenceParts=["Hello", "world", "this", "is", "great"];
+//a
+const combined = arr1.concat(arr2);
+console.log(combined);
+//b
+const sentence = sentenceParts.join(" ");
+console.log(sentence);
+//c
+const middleThree = sentenceParts.slice(1, 4);
+console.log(middleThree);
+
