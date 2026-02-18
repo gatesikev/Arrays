81c7c0540a6db2a90fddcd739a26bc5b3278a006
gatesikev <gatesikev1@gmail.com>
2026-02-17 22:58:27 -0800

    Question 7

--- index.js
+++ index.js
@@ -97,3 +97,19 @@ console.log(userAges.every(age => age >= 18));
 //c
 console.log(userAges.some(age => age % 5 === 0));
 
+
+//SECTION C: Transformation and Reordering
+//Exercise 7: Sorting and Reversing
+
+const dataPoints=[42, 10, 500, 2, 77];
+//a
+const ascendingSorted = [...dataPoints].sort((a, b) => a - b); // sorts in ascending order
+console.log(ascendingSorted);
+
+//b
+const descendingSorted = [...dataPoints].sort((a, b) => b - a); // sorts in descending order
+console.log(descendingSorted);
+//c
+const reversed = [...dataPoints].reverse(); // reverses the original array order
+console.log(reversed);
+
