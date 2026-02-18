f81f0c75f938a8807a3f0ef3ced85ce775e322fd
gatesikev <gatesikev1@gmail.com>
2026-02-18 01:24:53 -0800

    Question 15

--- index.js
+++ index.js
@@ -196,3 +196,20 @@ console.log(userNames);
 
 const usersOlderThan28 = users.filter(user => user.age > 28);
 console.log(usersOlderThan28);
+
+// Section F: Multidimensional Arrays (Matrices)
+//Exercise 15: Creating and Accessing a Matrix
+//a
+let gameBoard = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+console.log(gameBoard);
+
+//b
+gameBoard[1][1] = 1; // Place a marker in the center
+console.log(gameBoard);
+
+//c
+console.log(gameBoard[0][2]);
