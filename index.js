ed004d3e54962c323f6b3d6dde8c131724ee5146
gatesikev <gatesikev1@gmail.com>
2026-02-17 23:33:36 -0800

    Question 11

--- index.js
+++ index.js
@@ -146,3 +146,15 @@ console.log(pricesEUR);
 //b
 const priceStrings = pricesUSD.map(price => `Item price: $${price.toFixed(2)}`);
 console.log(priceStrings);
+
+// Exercise 11: Filtering Data(filter)
+
+const testScores = [45, 78, 92, 30, 65, 88];
+//a
+const passingScores = testScores.filter(score => score >= 70);
+console.log(passingScores);
+//b
+const average = testScores.reduce((sum, score) => sum + score, 0) / testScores.length;
+const belowAverageScores = testScores.filter(score => score < average);
+console.log(belowAverageScores);
+                                                                                                                                      
