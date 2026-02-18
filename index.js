67a72f3619b9094ff240cc54386945c41477cbe2
gatesikev <gatesikev1@gmail.com>
2026-02-17 16:14:11 -0800

    Question 4

--- index.js
+++ index.js
@@ -55,5 +55,21 @@ spreadCopy[0]= 50;
 console.log("Original Scores:", originalScores);
 console.log("Spread Copy:", spreadCopy);
 
+//SECTION 3: Searching and Checking
+//Exercise 4: Finding Elements by Index and Value
+
+const studentNames= ["Alice", "Bob", "Charlie", "Alice", "David"];
+
+//a 
+//the indexof, lastindex = if the first condition is successful it print out true then the condition stops
+console.log(studentNames.indexOf("Alice"));
+//b
+console.log(studentNames.lastIndexOf("Alice"));
+//c
+console.log(studentNames.includes("Charlie"));
+//d
+console.log(studentNames.includes("Eve"));
+
+
 
 
